c3c04fdd6826179a32b1ebac8ecdef28b19aa8aa
Pau <pau.rcabre@gmail.com>
2022-10-26 17:45:25 +0200

    Clock

--- src/Clock.java
+++ src/Clock.java
@@ -1,8 +1,31 @@
+import java.time.LocalDateTime;
 import java.util.Timer;
 import java.util.TimerTask;
-public class Clock extends Observable{
+
+public class Clock extends java.util.Observable{
     private TimerTask timerTask;
     private Timer timer;
-    public void startTimer(){}
-    private void tick(){}
+    private static Clock uniqueInstance = null;
+
+    public Clock() {
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                tick();
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+
+    }
+    private void tick(){
+        setChanged();
+        notifyObservers(LocalDateTime.now());
+    }
+    public static Clock getInstance() {
+        if(uniqueInstance == null)
+            uniqueInstance = new Clock();
+        return uniqueInstance;
+    }
+
 }
--- src/Observable.java
+++
@@ -1,6 +0,0 @@
-public abstract class  Observable{
-    public void addObserver(Observer ob){}
-    public void deleteObserver(Observer ob){}
-    public void setChanged(){}
-    public void notifyObservers(Observable arg0, Object arg1){}
-}
