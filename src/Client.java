f0ffc3eebeb1df2e6c5d7f5ca432d5929061f6c8
Paul <paul.ibiza2002@gmail.com>
2022-11-05 21:03:50 +0100

    Commit posible solucion json

--- src/Client.java
+++ src/Client.java
@@ -8,8 +8,8 @@ public class Client {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         //testA();
-        //testB();
-        testPersistence();
+        testB();
+        //testPersistence();
         //loadTest("file.json");
 
     }
@@ -122,6 +122,40 @@ public class Client {
         Project rootLoad = new Project(jsonObject, null);
 
     }
+/*
+    public static void getKey(JSONObject json, String key) {
+        boolean exists = json.has(key);
+        Iterator < ? > keys;
+        String nextKeys;
 
+        if (!exists) {
+            keys = json.keys();
+            while (keys.hasNext()) {
+                nextKeys = (String) keys.next();
+                try {
+                    if (json.get(nextKeys) instanceof JSONObject) {
+                        if (exists == false) {
+                            getKey(json.getJSONObject(nextKeys), key);
+                        }
+                    } else if (json.get(nextKeys) instanceof JSONArray) {
+                        JSONArray jsonarray = json.getJSONArray(nextKeys);
+                        for (int i = 0; i < jsonarray.length(); i++) {
+                            String jsonarrayString = jsonarray.get(i).toString();
+                            JSONObject innerJSOn = new JSONObject(jsonarrayString);
+                            if (exists == false) {
+                                getKey(innerJSOn, key);
+                            }
+                        }
+                    }
+                } catch (Exception e) {}
+            }
+        } else {
+            parseObject(json, key);
+        }
+    }
+
+    public static void parseObject(JSONObject json, String key) {
+        System.out.println("Key : "+key+" has value : "+json.get(key));
+    }*/
 }
 
